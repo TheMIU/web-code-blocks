6a9f5f893077db08550c0d815a16ea24ca6919f7
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-01-01 13:03:51 +0530

    update

--- disable-select/disable_select.js
+++ disable-select/disable_select.js
@@ -1,14 +1,4 @@
-// Create a function to disable text selection
-function disableTextSelection() {
-    // Function to prevent default action for mouse events
-    function preventDefaultAction(event) {
-        event.preventDefault();
-    }
-
-    // Disable text selection for mouse events
-    document.addEventListener('selectstart', preventDefaultAction); // For selection with the mouse
-    document.addEventListener('mousedown', preventDefaultAction); // For general mousedown events
-}
-
-// Call the function to disable text selection
-disableTextSelection();
+// Prevent text selection
+document.addEventListener('selectstart', function (e) {
+    e.preventDefault();
+});

6e519d777d3da2cfd495b5eb50671bbfe00e4ba8
Kasun Miuranga <kasunmiuranga2000@gmail.com>
2024-04-05 14:46:29 +0530

    Horizontal Scroll

---
+++ horizontal-scroll/script.js
@@ -0,0 +1,28 @@
+const slider = document.querySelector('.slider')
+const sections = gsap.utils.toArray('.slider section')
+
+let tl = gsap.timeline({
+    defaults: {
+        ease: "none"
+    },
+    scrollTrigger: {
+        trigger: slider,
+        pin: true,
+        scrub: 2,
+        end: () => "+=" + slider.offsetWidth
+    }
+})
+
+tl.to(slider, {
+    xPercent: -66
+})
+
+// smooth scroll
+const lenis = new Lenis();
+
+function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
